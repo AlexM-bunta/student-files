eaa77813d93f828540c2458be27ce57ac4489e70
AlexM23 <alexandrumitran23@gmail.com>
2021-03-13 15:07:07 +0200

    added time ago function

--- includes/post_functions.php
+++ includes/post_functions.php
@@ -18,7 +18,9 @@
       $row = $stmt->fetchAll();
 
       foreach($row as $value){
-        array_push($arr_posts, new Post($value['title'], $value['author'], $value['body'], $value['id']));
+        $post = new Post($value['title'], $value['author'], $value['body'], $value['id']);
+        $post->setDate($value['date']);
+        array_push($arr_posts, $post);
       }
     }
 
@@ -41,22 +43,44 @@
     return $arr;
   }
 
-  function getTimeBetween($curr, $prev, $manner){
+  function getTimeBetween($curr, $prev){
     $curr = getDateAsArray($curr);
     $prev = getDateAsArray($prev);
 
-    print_r($curr);
+    // print_r($curr);
+    // print_r($prev);
+
+    if ($curr['y'] > $prev['y']){
+      return $curr['y'] - $prev['y'] . " year" . (($curr['y'] - $prev['y'] > 1) ? "s" : "") . " ago";
+    }
+    else if ($curr['m'] > $prev['m']){
+      return $curr['m'] - $prev['m'] . " month" . (($curr['m'] - $prev['m'] > 1) ? "s" : "") . " ago";
+    }
+    else if ($curr['d'] > $prev['d']){
+      return $curr['d'] - $prev['d'] . " day" . (($curr['d'] - $prev['d'] > 1) ? "s" : "") . " ago";
+    }
+    else if ($curr['h'] > $prev['h']){
+      return $curr['h'] - $prev['h'] . " hour" . (($curr['h'] - $prev['h'] > 1) ? "s" : "") . " ago";
+    }
+    else if ($curr['i'] > $prev['i']){
+      return $curr['i'] - $prev['i'] . " minute" . (($curr['i'] - $prev['i'] > 1) ? "s" : "") . " ago";
+    }
+    else if ($curr['s'] > $prev['s']){
+      return $curr['s'] - $prev['s'] . " second" . (($curr['s'] - $prev['s'] > 1) ? "s" : "") . " ago";
+    }
+
   }
 
   function show_post($post, $userSESSION){
 
     $viewuser = ($post->getAuthor() !== $userSESSION) ? '&viewuser=' . $post->getAuthor() : '';
-    $time = getTimeBetween(date("h:i:s-d:m:y"),$post->getDate(), 'h');
+    $time = getTimeBetween(date("h:i:s-d:m:y"),$post->getDate());
 
     echo '<div>
       <h3>' . $post->getTitle() . '</h3>
       <p></p>
       <p>by <a href="/views/profile.php?username=' . $userSESSION . $viewuser . '">' . $post->getAuthor() . '</a></p>
+      <p>'.$time.'</p>
       <p>' . $post->getBody() . '</p>
     </div> <br />';
 
